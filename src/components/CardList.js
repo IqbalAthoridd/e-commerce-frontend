23b2f280d28c7f8249f85373254c0bc0a6c68083
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-19 04:40:21 +0100

    created UIfor alert from reactstarp on CardList

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -46,29 +46,49 @@ const CardList = () => {
   };
 
   return (
-    <Row className="mt-3">
-      {items === undefined
-        ? ''
-        : items.map((item) => {
-            return (
-              <Col key={item.id} sm="6" md="3" xs="6">
-                <Card body className=" p-0 p-2 pl-3 shadow mb-2">
-                  <CardTitle>{item.name}</CardTitle>
-                  <CardText>{item.price}</CardText>
-                  <CardText>{item.description}</CardText>
-                  <ButtonGroup>
-                    <Button color="primary" onClick={() => toggle(item.id)}>
-                      Edit
-                    </Button>
-                    <Button color="danger" onClick={() => deleteItem(item.id)}>
-                      Delete
-                    </Button>
-                  </ButtonGroup>
-                </Card>
-              </Col>
-            );
-          })}
-    </Row>
+    <>
+      <Row className="justify-content-center position-relative mt-5">
+        <Col md="3" xs="4" sm="3 text-center position-absolute">
+          {updated.alert === true ? (
+            <Alert className="rounded p-0 p-1 m-0" color="success">
+              {updated.message}!
+            </Alert>
+          ) : (
+            <Row className="justify-content-center">
+              <Col className=" p-0 p-3 "></Col>
+            </Row>
+          )}
+        </Col>
+        {updated.alert === true ? closeAlert() : ''}
+      </Row>
+
+      <Row className="mt-3">
+        {items === undefined
+          ? ''
+          : items.map((item) => {
+              return (
+                <Col key={item.id} sm="6" md="3" xs="6">
+                  <Card body className=" p-0 p-2 pl-3 shadow mb-2">
+                    <CardTitle>{item.name}</CardTitle>
+                    <CardText>{item.price}</CardText>
+                    <CardText>{item.description}</CardText>
+                    <ButtonGroup>
+                      <Button color="primary" onClick={() => toggle(item.id)}>
+                        Edit
+                      </Button>
+                      <Button
+                        color="danger"
+                        onClick={() => deleteItem(item.id)}
+                      >
+                        Delete
+                      </Button>
+                    </ButtonGroup>
+                  </Card>
+                </Col>
+              );
+            })}
+      </Row>
+    </>
   );
 };
 export default CardList;
