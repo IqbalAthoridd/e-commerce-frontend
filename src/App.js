b482ece53e2d7019341b91852cafd962084cf5c1
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-19 08:38:53 +0100

    Added route Login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dasboard from './pages/Dasboard';
 import Home from './pages/Home';
+import Login from './pages/Login';
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +12,7 @@ class App extends Component {
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/Dasboard" component={Dasboard} />
+          <Route path="/Login" component={Login} />
         </Switch>
       </BrowserRouter>
     );
