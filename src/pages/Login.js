e576f9f3aa4f77e7c1da76a47428d0c3c929cc31
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-19 08:35:08 +0100

    create file Login.js

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import FormLogin from '../components/Form/Login';
+class Login extends Component {
+  render() {
+    return (
+      <>
+        <FormLogin />
+      </>
+    );
+  }
+}
+
+export default Login;
