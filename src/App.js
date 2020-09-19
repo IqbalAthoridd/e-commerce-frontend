e766e6c24c09b16931bc54fa98a9373d96231bd2
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-19 04:11:32 +0100

    created route Home and dasboard on App.js using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    
-  );
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Dasbord from './pages/Dasbord';
+import Home from './pages/Home';
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/Dasbord" component={Dasbord} />
+          <Route path="/" component={Home} exact />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
