b60944ea0f5b2029b144333790999cddbe8874f3
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-10 13:42:53 +0100

    created Product Informasion

---
+++ src/components/Produk Details/Informasi Produk/stylesInformation.js
@@ -0,0 +1,10 @@
+import {makeStyles} from '@material-ui/core'
+
+export const styleInformation = makeStyles({
+    lContainer:{
+        margin:"40px 0"
+    },
+    cContainer:{
+        marginBottom:"40px"
+    }
+})
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -3,6 +3,8 @@ import {Grid} from '@material-ui/core'
 import styles from './pages.module.css'
 import NavigationBar from '../components/Navbar/NavigationBar'
 import ProductDetails from '../components/Produk Details/Details/ProductDetails'
+import ProductInformaton from '../components/Produk Details/Informasi Produk/ProductInformaton'
+
 
 
 class Details extends Component {
@@ -12,6 +14,7 @@ class Details extends Component {
             <NavigationBar/>
             <Grid container className={styles.container}>
                 <ProductDetails/>
+                <ProductInformaton/>
             </Grid>
             </>
         )
