74d2a914c83cde3a4d18178ce1eee632e451502d
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-12 10:53:00 +0100

    added component to app js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route,P } from 'react-router-dom';
 import Dasboard from './pages/Dasboard';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -10,6 +10,9 @@ import { Provider } from 'react-redux'
 import store from './redux/strore'
 import Profile from './pages/Profile'
 import Details from './pages/Details'
+import PrivateRoute from './components/Private Route/PrivateRoute'
+import ItemCategory from './pages/ItemCategory'
+import CheckOut from './pages/CheckOut'
 
 class App extends Component {
   render() {
@@ -19,9 +22,13 @@ class App extends Component {
         <Switch>
           <Route path="/" render={()=><Home/>} exact />
           <Route path="/dasboard" component={Dasboard} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/cart" render={()=><Cart/>} />
+          <Route path="/login" render={()=><Login/>} />
+          <Route path="/register" render={()=><Register/>} />
+          <Route path="/category" render={()=><ItemCategory/>}/>
+          <Route path="/checkout" render={()=><CheckOut/>} />
+          <PrivateRoute path="/cart">
+          <Cart/>
+          </PrivateRoute>
           <Route path="/profile" render={()=><Profile/>}></Route>
           <Route path="/details" render={()=><Details/>}/>
         </Switch>
