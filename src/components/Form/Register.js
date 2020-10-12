399e739e2850b62060631936f7d4d4c21b8f1799
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-12 10:21:08 +0100

    created Register using hooks

--- src/components/Form/Register.js
+++ src/components/Form/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 
 import Logo from '../../assets/img/Logo.svg';
 import {
@@ -11,110 +11,83 @@ import {
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
-const useStyles = makeStyles({
-  textCenter: {
-    textAlign: 'center',
-    marginBottom: '40px',
-  },
-  textRight: {
-    textAlign: 'right',
-    marginBottom: '40px',
-    marginTop: '20px',
-  },
-  image: {
-    maxWidth: 140,
-  },
-  buttonCon: {
-    marginLeft: '20%',
-    marginBottom: '20px',
-  },
-  marginLogin: {
-    marginBottom: '12px',
-  },
-  marginImage: {
-    marginBottom: '40px',
-  },
-  marginContainer: {
-    marginTop: '4%',
-  },
-  marginInput: {
-    marginBottom: '10px',
-  },
-  marginbtnLogin: {
-    marginTop: '40px',
-    textAlign: 'center',
-  },
-  ltext: {
-    fontWeight: '600',
-  },
-  forgotPass: {
-    fontSize: '15px',
-    color: '#3285A8',
-  },
-  textLink: {
-    textDecoration: 'none',
-    color: '#3285A8',
-  },
-});
-
-const ButtonStyle = makeStyles({
-  root: {
-    '&:focus': {
-      outline: 'none',
-    },
-  },
-  login: {
-    borderRadius: '20px',
-    '&:focus': {
-      outline: 'none',
-    },
-  },
-  Color: {
-    background: '#ffff',
-    color: '#9b9b9',
-    fontSize: '15px',
-    textTransform: 'lowercase',
-    '&:hover': {
-      background: '#326da8',
-      color: '#FFFF',
-    },
-    '&:focus': {
-      background: '#326da8',
-      outline: 'none',
-      color: '#FFFF',
-    },
-    '&:active': {
-      background: '#326da8',
-      outline: 'nonde',
-      color: '#FFFF',
-    },
-  },
-  Login: {
-    borderRadius: '20px',
-    background: '#3285A8',
-    color: '#FFF',
-    '&:hover': {
-      background: '#326da8',
-      color: '#FFFF',
-    },
-    '&:focus': {
-      background: '#3285A8',
-      outline: 'none',
-      color: '#FFFF',
-    },
-    '&:active': {
-      background: '#3285A8',
-      outline: 'nonde',
-      color: '#FFFF',
-    },
-  },
-});
+import { useStyles, ButtonStyle } from './formStles';
+import {useHistory} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import registerAction from '../../redux/action/register'
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 const Register = () => {
   const classes = useStyles();
-  const button = ButtonStyle();
+  const button = ButtonStyle(); 
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const [form,setform] = useState(true)
+  const [inputCustommer, setInputCustommer] = useState({name:"",email:"",password:""})
+  const [inputSeller, setInputSeller] = useState({name:"",email:"",password:"",phoneNumber:""})
+  const register = useSelector(state=>state.register)
+
+
+  useEffect(()=>{
+    if(!register.isError){
+      setInputCustommer({name:"",email:"",password:""})
+    }
+  },[register.isError])
+
+  const changeForm =()=>{
+    setform(!form)
+  }
+
+  const registerCustommer =(e)=> {
+    e.preventDefault();
+    const {name,email,password} = inputCustommer
+    let data = {
+      name,
+      email,
+      password
+    }
+    dispatch(registerAction.register(data))
+  }
+
+  const onChangeText = (e)=>{
+    setInputCustommer({...inputCustommer ,[e.target.name]:e.target.value})
+  }
+
+  function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+  
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
     <>
+    {register.isLoading && (
+      <Backdrop  open={true}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
+    )}
+
+    {register.isError && (
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        {console.log("ok")}
+      <Alert onClose={handleClose} severity="success">
+        This is a success message!
+      </Alert>
+    </Snackbar>
+    )}
+    
       <Grid container justify="center">
         <Grid
           item
@@ -138,15 +111,58 @@ const Register = () => {
           </Grid>
           <Grid item md={8} xs={7} className={classes.buttonCon}>
             <ButtonGroup variant="outlined" size="large" fullWidth="true">
-              <Button className={button.Color}>Custommer</Button>
-              <Button variant="outlined" className={button.Color}>
+            {form===true?(
+                 <Button variant="outlined"  className={button.Custommer}>
+                 Custommer
+               </Button>
+              ):(
+                <Button variant="outlined" onClick={changeForm} className={button.Custommer2} active>
+                 Custommer
+               </Button>
+              )}
+                <Button variant="outlined" onClick={form===true&&changeForm} className={button.Seller}>
                 Seller
               </Button>
             </ButtonGroup>
           </Grid>
           <Grid item md={12} xs={12} className={classes.textCenter}>
             <Grid item md={12} xs={12} sm={12}>
-              <from>
+              {form?(
+                <form onSubmit={registerCustommer}>
+                <FormControl fullWidth="true" className={classes.marginInput}>
+                  <InputLabel htmlFor="name">Name</InputLabel>
+                  <Input id="name" name="name" onChange={onChangeText} fullWidth="true" type="text" />
+                </FormControl>
+                <FormControl fullWidth="true" className={classes.marginInput}>
+                  <InputLabel htmlFor="email">Email address</InputLabel>
+                  <Input id="email" name="email" onChange={onChangeText} fullWidth="true" type="email" />
+                </FormControl>
+                <FormControl fullWidth="true">
+                  <InputLabel htmlFor="password">Password</InputLabel>
+                  <Input
+                    id="password"
+                    type="password"
+                    name="password"
+                    onChange={onChangeText}
+                    aria-describedby="my-helper-text"
+                    fullWidth="true"
+                  />
+                </FormControl>
+
+                <Grid md={12} className={classes.marginbtnLogin}>
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    fullWidth={true}
+                    className={button.Login}
+                    size="medium"
+                  >
+                    Register
+                  </Button>
+                </Grid>
+              </form>
+              ):(
+                <form>
                 <FormControl fullWidth="true" className={classes.marginInput}>
                   <InputLabel htmlFor="name">Name</InputLabel>
                   <Input id="name" fullWidth="true" type="text" />
@@ -154,6 +170,14 @@ const Register = () => {
                 <FormControl fullWidth="true" className={classes.marginInput}>
                   <InputLabel htmlFor="email">Email address</InputLabel>
                   <Input id="email" fullWidth="true" type="email" />
+                </FormControl>
+                <FormControl fullWidth="true" className={classes.marginInput}>
+                  <InputLabel htmlFor="phonenumber">Phone Number</InputLabel>
+                  <Input id="phonenumber" fullWidth="true" type="text" />
+                </FormControl>
+                <FormControl fullWidth="true" className={classes.marginInput}>
+                  <InputLabel htmlFor="storename">Store Name</InputLabel>
+                  <Input id="storename" fullWidth="true" type="text" />
                 </FormControl>
                 <FormControl fullWidth="true">
                   <InputLabel htmlFor="password">Password</InputLabel>
@@ -175,15 +199,17 @@ const Register = () => {
                     Register
                   </Button>
                 </Grid>
-              </from>
+              </form>
+              )}
+              
             </Grid>
           </Grid>
-          <Grid item md={12} className={classes.marginbtnLogin}>
+          <Grid item md={12} style={{textAlign:"center"}}>
             <span>
               Already have a warung account?{' '}
               <Link to="/Login">
                 {' '}
-                <span>Loginr</span>
+                <span>Login</span>
               </Link>
             </span>
           </Grid>
