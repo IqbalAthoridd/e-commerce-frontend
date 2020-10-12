42dec76ef04d13a59fd00754430feddb4eb70d7f
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-12 10:39:11 +0100

    added reducers to index

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,9 +3,17 @@ import {combineReducers} from 'redux'
 import newItems from './newItems'
 import listCategory from './listCategory'
 import popularProduct from './popularProduct'
+import auth from './auth'
+import register from './register'
+import details from './details'
+import listProduct from './listProduct'
 
 export default combineReducers({
     newItems,
     listCategory,
-    popularProduct
+    popularProduct,
+    auth,
+    register,
+    details,
+    listProduct
   })
