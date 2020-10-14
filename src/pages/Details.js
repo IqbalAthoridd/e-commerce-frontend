8a9d392c90c7e3db25326435cf62dd0fd083515f
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-14 16:05:23 +0100

    created fungtion auto scroll

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react'
-import {Grid} from '@material-ui/core'
-import styles from './pages.module.css'
-import NavigationBar from '../components/Navbar/NavigationBar'
-import ProductDetails from '../components/Produk Details/Details/ProductDetails'
-import RecomenList from '../components/Produk Details/Product Recomen/RecomenList'
+import React, { useEffect } from 'react';
+import { Grid } from '@material-ui/core';
+import styles from './pages.module.css';
+import NavigationBar from '../components/Navbar/NavigationBar';
+import ProductDetails from '../components/Produk Details/Details/ProductDetails';
+import RecomenList from '../components/Produk Details/Product Recomen/RecomenList';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Container from '@material-ui/core/Container'
+import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom'
 
+function Details() {
 
+    const isLoading = useSelector(state=>state.details.isLoading)
+    const location = useLocation()
 
-class Details extends Component {
-    render() {
-        return (
-            <>
-            <NavigationBar/>
-            <Grid container className={styles.container}>
-                <ProductDetails/>
-                <RecomenList/>
-            </Grid>
-            </>
-        )
-    }
+    useEffect(()=>{
+        window.scrollTo(0, 0);
+    },[location])
+
+  return (
+    <>
+        <NavigationBar />
+        <Grid container className={styles.container}>
+          <ProductDetails />
+          <RecomenList />
+        </Grid>
+    </>
+  );
 }
 
-export default Details
+export default Details;
