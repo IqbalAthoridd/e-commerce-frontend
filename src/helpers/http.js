730ff6f4f28697309fd4f413539ef93a846d4ffb
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-14 15:58:22 +0100

    delete unused variabel

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 const {REACT_APP_BACKEND_URL} = process.env
 
 const http =(token=false)=>{
-    console.log("ok",REACT_APP_BACKEND_URL)
     return axios.create({
         baseURL:REACT_APP_BACKEND_URL,
         headers: {
