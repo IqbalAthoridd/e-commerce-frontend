8b7499d4d015ead972d526a8717b511cd0d7a03c
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-14 16:02:57 +0100

    created ui custommer my account

--- src/components/Profile Custommer/stylesProfile.js
+++ src/components/Profile Custommer/stylesProfile.js
@@ -1,7 +1,55 @@
 import {makeStyles} from '@material-ui/core'
 
-export const profileStyle = makeStyles({
+export const profileStyle = makeStyles((theme)=>({
     sidebar:{
        height:"100%"
+    },
+    btnPerson:{
+        background:"#456BF3",
+        width: "32px",
+        height: "32px",
+        '&:focus': {
+            outline: 'none',
+          },
+          '&:hover': {
+            outline: 'none',
+            background:"#456BF3"
+          },
+    },
+    btnLocation:{
+        background:"#F36F45",
+        width: "32px",
+        height: "32px",
+        '&:focus': {
+            outline: 'none',
+          },
+          '&:hover': {
+            outline: 'none',
+            background:"#F36F45"
+          },
+    },
+    btnOrder:{
+        background:"#F3456F",
+        width: "32px",
+        height: "32px",
+        '&:focus': {
+            outline: 'none',
+          },
+          '&:hover': {
+            outline: 'none',
+            background:"#F3456F"
+          },
+    },
+    iconColor:{
+        color:"white"
+    },
+    large: {
+      width: theme.spacing(15),
+      height: theme.spacing(15),
+      fontSize:"80px",
+      textTransform:"uppercase"
+    },
+    input:{
+      display:"none"
     }
-})
+}))
