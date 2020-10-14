3f9530e518b13a680ae26f82b9cc5967326a074f
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-14 16:11:46 +0100

    craeted cart.js for added to cart

---
+++ src/redux/reducers/cart.js
@@ -0,0 +1,34 @@
+const initialState = {
+    isError: false,
+    alertMsg: '',
+    isLoading:false
+  }
+  
+  export default (state=initialState, action) => {
+    switch(action.type){
+      case 'ADD_CART_PENDING':{
+        return {
+          ...state,
+          isLoading: true
+        }
+      }
+      case 'ADD_CART_REJECTED': {
+        return {
+          ...state,
+          isLoading: false,
+          isError: true,
+          alertMsg: "Failed added "
+        }
+      }
+      case 'ADD_CART_FULFILLED':{
+        return {
+          ...state,
+          isLoading: false,
+          alertMsg: 'Added to cart'
+        }
+      }
+      default : {
+        return state
+      }
+    }
+  }
