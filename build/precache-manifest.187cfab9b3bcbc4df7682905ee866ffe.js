f77f0f91f6e62dd1faefd8854e12344bf5c02a57
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-17 16:51:01 +0100

    build

---
+++ build/precache-manifest.187cfab9b3bcbc4df7682905ee866ffe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d764f23a1682d1e546baf9744b48174d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74c4c4c311ca94a662c9",
+    "url": "/static/css/2.821b8ecc.chunk.css"
+  },
+  {
+    "revision": "593d574563daa64dee43",
+    "url": "/static/css/main.e242701e.chunk.css"
+  },
+  {
+    "revision": "74c4c4c311ca94a662c9",
+    "url": "/static/js/2.d90405af.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.d90405af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593d574563daa64dee43",
+    "url": "/static/js/main.42e31ed1.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "701dd39de9d09cb634a7d82ee90c70c2",
+    "url": "/static/media/Logo.701dd39d.svg"
+  },
+  {
+    "revision": "86eb98635233be43f682f26bad629f11",
+    "url": "/static/media/Rubik-Var.86eb9863.ttf"
+  },
+  {
+    "revision": "3b4833b1fb39ea2b089d0b399510a1d7",
+    "url": "/static/media/bajuko.3b4833b1.jpg"
+  },
+  {
+    "revision": "e8493ce5e6c75dcbd8d94185da707ef0",
+    "url": "/static/media/benjamin.e8493ce5.jpg"
+  },
+  {
+    "revision": "74f7713064201ba412d6b0d923d0c82f",
+    "url": "/static/media/tsirt.74f77130.png"
+  }
+]);
